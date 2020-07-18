179d244e96381079a671a0ac8fb8e3db4cffa241
Marc Brinkmann <git@marcbrinkmann.de>
2020-07-18 16:26:05 +0200

    Expose and document `Extras.Html`

--- src/Extras/Html.elm
+++ src/Extras/Html.elm
@@ -1,8 +1,27 @@
 module Extras.Html exposing (nothing)
 
+{-| Html extras
+
+Convenience functions for working with `Html.Html`
+
+@docs nothing
+
+-}
+
 import Html exposing (Html, text)
 
 
+{-| Empty element
+
+Useful if need to produce an HTML element that evaluates to nothing. Example:
+
+    let
+        optionalContent =
+            Just (div [] [ text "hello " ])
+    in
+    Maybe.withDefault nothing optionalContent
+
+-}
 nothing : Html msg
 nothing =
     text ""
