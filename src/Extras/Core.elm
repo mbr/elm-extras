d8c714315796366b2f84f5a7411619b95edd4945
Marc Brinkmann <git@marcbrinkmann.de>
2020-07-18 16:58:14 +0200

    Added `Core` module

---
+++ src/Extras/Core.elm
@@ -0,0 +1,50 @@
+module Extras.Core exposing
+    ( always
+    , flip
+    , unless
+    )
+
+{-| General convenience functions.
+
+@docs always
+@docs flip
+@docs unless
+
+-}
+
+
+{-| Regardless of input, always return a constant value.
+
+Helpful adapter for function that only offer updates when one really just wants to replace a value:
+
+    List.updateAt index (always item) list
+
+-}
+always : a -> b -> a
+always value =
+    \_ -> value
+
+
+{-| Change the order of the arguments of a two argument function.
+-}
+flip : (a -> b -> c) -> (b -> a -> c)
+flip f a b =
+    f b a
+
+
+{-| Negated functional `if-then-else`
+
+Checks a condition and returns the respective value.
+
+Allows writing more natural conditional atttributes, elements, etc:
+
+    div [] [ unless isLoggedIn (text "not logged in") (a [] [ text "logout" ]) ]
+
+-}
+unless : Bool -> a -> a -> a
+unless condition onFalse onTrue =
+    if condition then
+        onTrue
+
+    else
+        onFalse
